faf813700d6be0af0e7ee65aa26b9143f97b47d2
Andry <mixtubes@gmail.com>
2016-12-01 07:51:48 -0300

    Ajout structure Front

---
+++ src/Societe/FrontBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Societe\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SocieteFrontBundle:Default:index.html.twig');
+    }
+}
