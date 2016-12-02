0df9be6c81a6236b1c05bb970258f61b55159f11
Aina Sitraka n6 <aynsix@gmail.com>
2016-12-02 11:49:21 +0300

    reset

--- src/Societe/FrontBundle/SocieteFrontBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Societe\FrontBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SocieteFrontBundle extends Bundle
-{
-}
--- src/Societe/MainBundle/SocieteMainBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Societe\MainBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SocieteMainBundle extends Bundle
-{
-}
